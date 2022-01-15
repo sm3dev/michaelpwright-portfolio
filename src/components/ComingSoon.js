c3b05f1bd0fdcfdcbf351f9534f36d6682ef83b8
Michael Wright <info@saymoremultimedia.com>
2022-01-14 19:45:55 -0600

    mw-add-components-ComingSoonLanding-Contact added array for social links;added code to share web page url; setup MUI component containers and linked MUI chip buttons

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -1,46 +1,118 @@
-import { Email, GitHub, LinkedIn, Share } from "@mui/icons-material";
+import {
+    Devices,
+  Email,
+  GitHub,
+  LinkedIn,
+  Share,
+} from "@mui/icons-material";
 import {
   Box,
   Card,
   CardActions,
   CardContent,
-  Typography,
   Button,
   CardHeader,
   Chip,
   Avatar,
   Stack,
+  ListItem,
+  Typography,
+  Link,
 } from "@mui/material";
 import React from "react";
+import a from "sass-node/lib/a";
 
 export const ComingSoon = () => {
-  const [chipData, setChipData] = useState([
+  let pageURL = encodeURI(document.location.href);
+  const socialChips = [
     {
       key: 1,
       label: "Connect on LinkedIn",
       url: "https://www.linkedin.com/in/mpw?trk=public-profile-badge-profile-badge-view-profile-cta",
-      theIcon: "LinkedIn",
+      connectType: "LinkedIn",
     },
     {
       key: 2,
       label: "Follow on GitHub",
       url: "https://github.com/sm3dev",
-      theIcon: "GitHub",
+      connectType: "GitHub",
     },
     {
       key: 3,
       label: "Email Me",
       url: "mailto:mrwry7@gmail.com",
-      theIcon: "Email",
+      connectType: "Email",
     },
-  ]);
+  ];
 
   // use MUI Chips: https://mui.com/components/chips/#basic-chip
   return (
-    <div>
-      <Box>
-        <Card></Card>
+    <>
+      <Box
+        className="mainBox"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          p: 4,
+          alignItems: "center",
+        }}
+      >
+        <Card sx={{ maxWidth: 345, m: 1 }} className="card">
+          <CardContent className="card-content__block">
+            <Box className="card-header__block"
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                py: 2,
+                alignItems: "center",
+              }}
+            >
+              {" "}
+              <Devices fontSize="large" sx={{m:2}} />
+              <Typography variant="h5" component="div" className="card__title text__all-caps">Let's Connect</Typography>
+            </Box>
+            <Stack className="card-content__action-stack">
+              {socialChips.map((data) => {
+                let icon;
+
+                if (data.connectType === "LinkedIn") {
+                  icon = <LinkedIn />;
+                } else if (data.connectType === "GitHub") {
+                  icon = <GitHub />;
+                } else if (data.connectType === "Email") {
+                  icon = <Email />;
+                }
+
+                return (
+                  <ListItem key={data.key}>
+                    <Chip
+                      icon={icon}
+                      label={data.label}
+                      component="a"
+                      href={data.url}
+                      target="_blank"
+                      clickable
+                      color="info"
+                      size="large"
+                    />
+                  </ListItem>
+                );
+              })}
+            </Stack>
+          </CardContent>
+          <CardActions>
+            <Button component="a" href={pageURL} title="share my card">SHARE THIS CARD </Button>
+            <Button variant="contained">DOWNLOAD RÉSUMÉ</Button>
+          </CardActions>
+        </Card>
+        <Card>
+          <Typography>
+            Have you visited the NSS Cohort 51 Website built by me and my 3
+            UI/UX Front-End <del>teammates</del> wizards?
+          </Typography>
+        </Card>
       </Box>
-    </div>
+    </>
   );
 };
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { ComingSoon } from "./ComingSoon";
 
 export const Portfolio = () => {
   return (
     <div className="page">
+        <ComingSoon />
       <h1>Hi, this is my Portfolio!</h1>
-      
+
     </div>
   );
 };
