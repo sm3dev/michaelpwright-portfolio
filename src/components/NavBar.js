d1803e6eea4a3a89ff3b17128d2f7914b06e057a
Michael Wright <info@saymoremultimedia.com>
2022-01-17 19:34:01 -0600

    mw-create-NavBars-20 wrote basic html for the mobile navbar and the logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
 
 export const NavBar = () => {
-    return (
+  return (
+    <div className="nav__block">
+      <section className="logo_block">
         <div>
-            
+          <img src="./" alt="" className="nav__logo" />
         </div>
-    )
-}
+        <p className="nav__tag-line">
+          Michael Wright is a UI/UX Front-End Developer based in Huntsville.
+        </p>
+      </section>
+      <section className="">
+        <ul class="">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">
+              Active
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Link
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Link
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">Disabled</a>
+          </li>
+        </ul>
+      </section>
+    </div>
+  );
+};
