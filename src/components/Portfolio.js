93c830e3b0f74f6f9e708683b4883afb6aa02dce
Michael Wright <info@saymoremultimedia.com>
2022-01-18 16:10:22 -0600

    mw-create-Footer-21 Built the Footer; began the Resume component

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,12 +2,14 @@ import React from "react";
 import { ComingSoon } from "./ComingSoon";
 import { Footer } from "./Footer";
 import { NavBar } from "./NavBar";
+import { Resume } from "./Resume";
 
 export const Portfolio = () => {
   return (
     <div className="page">
       <NavBar />
       <ComingSoon />
+      <Resume />
       <Footer />
     </div>
   );
