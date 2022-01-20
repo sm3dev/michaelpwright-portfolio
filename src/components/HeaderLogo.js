2de6420bdaf758b7b86c51e32f1e8aa1ea52fbd9
Michael Wright <info@saymoremultimedia.com>
2022-01-20 15:55:37 -0600

    mw-create-header-logo-component-31 In NavBar, I removed the code for the HeaderLogo; the navbar is now only a block with nav items, so I can use it where I need it without the website logo or tagline text.

--- src/components/HeaderLogo.js
+++ src/components/HeaderLogo.js
@@ -9,15 +9,15 @@ const HeaderLogo = ({ allNavTaglines }) => {
       <div className="logo__block">
         {" "}
         <img
-          src={require(`../images/mpwMinecraft3DGoldLogo512.png`).default}
+          src="logo512.png"
           alt="MPW website logo"
           className="header__logo"
         />{" "}
       </div>
       <div className="nav-tagline__block">
         {" "}
-        {/* This has to has to map through the array object allNavTaglines */}
-        <p className="nav-tagline__text">text should be here</p>
+        {/* This has to has to map through the array object allNavTaglines. Helper function will perform this -- Issue #32 */}
+        <p className="nav-tagline__text">text should be here: {allNavTaglines[3]?.tagline}</p>
       </div>
       <NavBar />
     </section>
