eda2218963f549a2c5a5a3164e5eb0a50c9d889d
Michael Wright <info@saymoremultimedia.com>
2022-01-21 13:39:22 -0600

    mw-create-techStack-component-26 added conditional code that returns a string; the string returned is used as the className for the tech list items element; the additional class is named usedHere.

--- src/components/TechStack.js
+++ src/components/TechStack.js
@@ -1,33 +1,29 @@
 import React from "react";
 
 export const TechStack = ({ allTechObjects }) => {
-  let techUsedArray = [1, 4, 5, 6, 8, 12, 20, 25, 26];
+  const techUsedArray = [1, 4, 5, 6,8,10,20,27,35];
 
-  const getMatch = (techObj) => {
-      let techItem = ""
-    // If any allTechObjects.id value matches an integer in array techUsedArray,
-    // then display the list item with the added class "usedHere".
-    // If not, display the regular list item
-    techUsedArray.find((data) => {
-      allTechObjects.id === data ? (
-        techItem = <li key={techObj.id} className="tech__item usedHere">
-          {techObj.name}
-        </li>
-      ) : (
-        techItem = <li key={techObj.id} className="tech__item">
-          {techObj.name}
-        </li>
-      );
-    });
-
-    return console.log("Bears");
+  // If any allTechObjects.id value matches an integer in array techUsedArray,
+  // then display the list item with the added class "usedHere".
+  // If not, display the regular list item
+  const getMatch = (id) => {
+    let techItem = techUsedArray.find((item) => item === id);
+    if (techItem) {
+      return "tech__item usedHere";
+    } else {
+      return "tech__item";
+    }
   };
 
   return (
     <section className="technologies__block">
       <p className="technologies__title">Technologies Used Here</p>
       <ul className="technologies__list">
-        {allTechObjects.map((techObj) => getMatch(techObj))}
+        {allTechObjects.map((techObj) => (
+          <li key={techObj.id} className={getMatch(techObj.id)}>
+            {techObj.name}
+          </li>
+        ))}
       </ul>
     </section>
   );
