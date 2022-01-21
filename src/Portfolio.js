1657e57984d240d4bc2e67974dc402a1857738f3
Michael Wright <info@saymoremultimedia.com>
2022-01-21 10:57:46 -0600

    mw-create-techStack-component-26 added selfie image to images directory; linked selfie image in the database; wrote function that maps through list of techSack objects and inserts them into List Item elements; in Portfolio, imported the TechStack, wrote useState code to create an array of the techstack objects

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -3,20 +3,26 @@ import { ComingSoon } from "./components/ComingSoon";
 import { Footer } from "./components/Footer";
 import HeaderLogo from "./components/HeaderLogo";
 import { Resume } from "./components/Resume";
+import { TechStack } from "./components/TechStack";
 
 export const Portfolio = () => {
   const [allNavTaglines, setAllNavTaglines] = useState([]);
+  const [allTechObjects, setAllTechObjects] = useState([]);
 
   useEffect(() => {
-    fetch("api/database.json").then(res => res.json()).then(data => {
+    fetch("api/database.json")
+      .then((res) => res.json())
+      .then((data) => {
         console.log(data.navTaglines);
         setAllNavTaglines(data.navTaglines);
+        setAllTechObjects(data.techStack);
       });
   }, []);
 
   return (
     <div className="page">
       <HeaderLogo allNavTaglines={allNavTaglines} />
+      <TechStack allTechObjects={allTechObjects} />
       <ComingSoon />
       <Resume />
       <Footer />
--- src/components/TechStack.js
+++ src/components/TechStack.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
-export const TechStack = () => {
-    return (
-        <section>
-            
-        </section>
-    )
-}
+export const TechStack = ({ allTechObjects }) => {
+  return (
+    <section className="technologies__block">
+      <p className="technologies__title">Technologies Used Here</p>
+      <ul className="technologies__list">
+        {allTechObjects.map((techObj) => (
+          <li key={techObj.id} className="tech__item">{techObj.name}</li>
+        ))}
+      </ul>
+    </section>
+  );
+};
