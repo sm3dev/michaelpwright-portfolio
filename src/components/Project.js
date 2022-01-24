c05879d1e81263883b1d05c3f4f0e55047c33b18
Michael Wright <info@saymoremultimedia.com>
2022-01-24 15:24:18 -0600

    mw-create-ProjectView-46 got all of the data to show in the DOM through the ProjectDetail.js and Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -19,11 +19,21 @@ export const Project = ({ projectObj }) => {
           <img src="placeholder-video.gif" alt="" className="project__video" />
         </section>
         <section className="project-links-button__block">
-          <a href={projectObj.linkToSite} className="project-button__link" target="_blank" title="Visit live website">
+          <a
+            href={projectObj.linkToSite}
+            className="project-button__link"
+            target="_blank"
+            title="Visit live website"
+          >
             {" "}
             <button className="project-links__button">Live Site</button>
           </a>
-          <a href={projectObj.linkRepo} className="project-button__link" target="_blank" title="Visit GitHub Repository">
+          <a
+            href={projectObj.linkRepo}
+            className="project-button__link"
+            target="_blank"
+            title="Visit GitHub Repository"
+          >
             {" "}
             <button className="project-links__button">GitHub Repo</button>
           </a>
@@ -41,49 +51,63 @@ export const Project = ({ projectObj }) => {
         />
         <figcaption>
           {" "}
-          <a href={projectObj.challengeImageLink} className="full-size-image__link" target="_blank" title="View High Res Image in New Tab">
+          <a
+            href={projectObj.challengeImageLink}
+            className="full-size-image__link"
+            target="_blank"
+            title="View High Res Image in New Tab"
+          >
             See Full Resolution
           </a>
         </figcaption>
       </figure>
       <h2 className="challenge__heading">{projectObj.challengeHeading}</h2>
-      <p className="challenge__text">
-        {projectObj.challengeText}
-      </p>
+      <p className="challenge__subtitle heading__subtitle">The Challenge</p>
+      <p className="challenge__text">{projectObj.challengeText}</p>
       <hr className="section__divider" />{" "}
       <figure className="goals-image__block">
-        <img src="logo192.png" alt="" className="goals__image" />
+        <img
+          src={require(`../images/${projectObj.goalsImage}`).default}
+          alt="goals and constraints image"
+          className="goals__image"
+        />
         <figcaption>
           {" "}
-          <a href="https://google.com" className="full-size-image__link" target="_blank" title="View High Res Image in New Tab">
+          <a
+            href={projectObj.goalsImageLink}
+            className="full-size-image__link"
+            target="_blank"
+            title="View High Res Image in New Tab"
+          >
             See Full Resolution
           </a>
         </figcaption>
       </figure>
-      <h2 className="goals__heading">Goals and Constraints</h2>
-      <p className="goals__text">
-        The Cohort website is a staple in the NSS Web Developer Program
-        cirriculum. The UI/UX Students of Cohort 51 were charged with creating a
-        hub for getting students employment and showcasing the UI/UX knowledge
-        and talent.
-      </p>
+      <h2 className="goals__heading">{projectObj.goalsHeading}</h2>
+      <p className="goals__subtitle heading__subtitle">Goals and Constraints</p>
+      <p className="goals__text">{projectObj.goalsText}</p>
       <hr className="section__divider" />{" "}
       <figure className="process-image__block">
-        <img src="logo192.png" alt="" className="process__image" />
+        <img
+          src={require(`../images/${projectObj.processImage}`).default}
+          alt="project process image "
+          className="process__image"
+        />
         <figcaption>
           {" "}
-          <a href="https://google.com" className="full-size-image__link" target="_blank" title="View High Res Image in New Tab">
+          <a
+            href={projectObj.processImageLink}
+            className="full-size-image__link"
+            target="_blank"
+            title="View High Res Image in New Tab"
+          >
             See Full Resolution
           </a>
         </figcaption>
       </figure>
-      <h2 className="process__heading">Our Process</h2>
-      <p className="process__text">
-        The Cohort website is a staple in the NSS Web Developer Program
-        cirriculum. The UI/UX Students of Cohort 51 were charged with creating a
-        hub for getting students employment and showcasing the UI/UX knowledge
-        and talent.
-      </p>
+      <h2 className="process__heading">{projectObj.processHeading}</h2>
+      <p className="process__subtitle heading__subtitle">The Process</p>
+      <p className="process__text">{projectObj.processText}</p>
     </section>
   );
 };
