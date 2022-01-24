eb71292ea7120348fe805df8281355f5e88eff05
Michael Wright <info@saymoremultimedia.com>
2022-01-24 14:43:46 -0600

    mw-create-ProjectView-46 added title and target attributes to links in Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,21 +6,24 @@ export const Project = ({ projectObj }) => {
       <section className="project-overview__section">
         <h1 className="project__name">{projectObj.name}</h1>
         <div className="desktopMobile-image__block">
-          <img src="" alt="" className="desktopMobile__image" />
+          <img
+            src={require(`../images/${projectObj.thumbnail}`).default}
+            alt={`${projectObj.name} thumbnail image`}
+            className="desktopMobile__image"
+          />
         </div>
-        <p className="project__description">
-          Break the internet with the first ever ever created
-        </p>
-        {/* This could be a good place for the demo video. Ideally the video would sit above the fold */}
+        <p className="project__description">{projectObj.description}</p>
         <section className="project-video__iframe">
+          {/* This could be a good place for the demo video. Ideally the video would sit above the fold */}
+
           <img src="placeholder-video.gif" alt="" className="project__video" />
         </section>
         <section className="project-links-button__block">
-          <a href="#" className="project-button__link">
+          <a href={projectObj.linkToSite} className="project-button__link" target="_blank" title="Visit live website">
             {" "}
             <button className="project-links__button">Live Site</button>
           </a>
-          <a href="#" className="project-button__link">
+          <a href={projectObj.linkRepo} className="project-button__link" target="_blank" title="Visit GitHub Repository">
             {" "}
             <button className="project-links__button">GitHub Repo</button>
           </a>
@@ -31,30 +34,29 @@ export const Project = ({ projectObj }) => {
       </section>
       <hr className="section__divider" />{" "}
       <figure className="challenge-image__block">
-        <img src="" alt="" className="challenge__image" />
+        <img
+          src={require(`../images/${projectObj.challengeImage}`).default}
+          alt=""
+          className="challenge__image"
+        />
         <figcaption>
           {" "}
-          <a href="https://google.com" className="full-size-image__link">
-            See full size
+          <a href={projectObj.challengeImageLink} className="full-size-image__link" target="_blank" title="View High Res Image in New Tab">
+            See Full Resolution
           </a>
         </figcaption>
       </figure>
-      <h2 className="challenge__heading">Showcase NSS C51 Students</h2>
+      <h2 className="challenge__heading">{projectObj.challengeHeading}</h2>
       <p className="challenge__text">
-        The Cohort website is a staple in the NSS Web Developer Program
-        cirriculum.
-        <br />
-        The UI/UX Students of Cohort 51 were charged with creating a hub for
-        getting students employment and showcasing the UI/UX knowledge and
-        talent.
+        {projectObj.challengeText}
       </p>
       <hr className="section__divider" />{" "}
       <figure className="goals-image__block">
         <img src="logo192.png" alt="" className="goals__image" />
         <figcaption>
           {" "}
-          <a href="https://google.com" className="full-size-image__link">
-            See full size
+          <a href="https://google.com" className="full-size-image__link" target="_blank" title="View High Res Image in New Tab">
+            See Full Resolution
           </a>
         </figcaption>
       </figure>
@@ -70,8 +72,8 @@ export const Project = ({ projectObj }) => {
         <img src="logo192.png" alt="" className="process__image" />
         <figcaption>
           {" "}
-          <a href="https://google.com" className="full-size-image__link">
-            See full size
+          <a href="https://google.com" className="full-size-image__link" target="_blank" title="View High Res Image in New Tab">
+            See Full Resolution
           </a>
         </figcaption>
       </figure>
