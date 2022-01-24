0b6c0a62b5ec27d451261a3f30743402eb586cfc
Michael Wright <info@saymoremultimedia.com>
2022-01-23 22:09:38 -0600

    mw-create-techSkillLevels-51 wrote the code for the tech stack skill levels; the list and meters write to the DOM; meter doesn't show numbers or hash marks, so I'm going to leave the meter levels up to the user's imagination

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -22,7 +22,8 @@ export const Portfolio = () => {
 
   return (
     <div className="page">
-<ProjectList />
+      <TechSkillLevels allTechObjects={allTechObjects} />
+      <ProjectList />
       <TechStack allTechObjects={allTechObjects} />
       <HeaderLogo allNavTaglines={allNavTaglines} />
       <ComingSoon />
@@ -32,4 +33,31 @@ export const Portfolio = () => {
   );
 };
 
-// export default Portfolio;
+export const TechSkillLevels = ({ allTechObjects }) => {
+  return (
+    <section id="tech-skill-levels__block">
+      <ul className="tech__list list--no-padding-inline-left">
+        {allTechObjects.map((techObj) => (
+          <li className="tech__list-item list-item__no-bullet text__white-space--no-wrap" key={techObj.id}>
+            {" "}
+            <div className="tech-list-item__container">
+              <label htmlFor={`technology-${techObj.id}`}>
+                {techObj.name}:&nbsp;{" "}
+              </label>
+              <meter className="skill__meter"
+                id={`technology-${techObj.id}`}
+                max="5"
+                low="1"
+                high="5"
+                optimum="3"
+                value={techObj.skillLevel}
+              >
+                Skill Level &nbsp;{techObj.skillLevel}
+              </meter>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
