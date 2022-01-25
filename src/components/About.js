5dcb286d75c6203b6d57959c196d03da5f633f01
Michael Wright <info@saymoremultimedia.com>
2022-01-25 09:55:00 -0600

    mw-create-About-component-59 Database Added properties for About introduction text, photo collage, photo collage full resolution link, accolades introductory text

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const About = () => {
     return (
-        <div>
-            
-        </div>
+        <section className='about__content'>
+            <div className="about-headshot__block"><img src={require("../images/michaelpwright-selfie-square-800px.jpg").default} alt="Michael P Wright headshot" className="about-headshot__image" /></div>
+        </section>
     )
 }
