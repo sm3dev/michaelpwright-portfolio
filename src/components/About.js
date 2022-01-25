6f736c3aec514c6a45fabe23871f494f7b328e2c
Michael Wright <info@saymoremultimedia.com>
2022-01-25 14:23:32 -0600

    mw-create-About-component-59 completed the HTML for the About; wrote HTML structure for Quotes and Testimonials

--- src/components/About.js
+++ src/components/About.js
@@ -21,7 +21,7 @@ export const About = () => {
         <div className="about-headshot__block">
           <img
             src={`../images/${aboutUser.headshot}`}
-            alt="Michael P Wright headshot"
+            alt={`${aboutUser.displayName} headshot`}
             className="about-headshot__image"
           />
         </div>
@@ -32,6 +32,75 @@ export const About = () => {
             {aboutUser.jobTitle}
           </p>
           <p className="about__intro-text">{aboutUser.aboutMeIntro}</p>
+        </section>
+
+        <figure className="photo-collage__block">
+          <img
+            src={`../images/${aboutUser.photoCollage}`}
+            alt={`${aboutUser.displayName} personal photo collage`}
+            className="photo-collage__image"
+          />
+          <figcaption>
+            {aboutUser.firstName}'s Personal Photos &#124;{" "}
+            <a
+              href={aboutUser.photoCollageLink}
+              className="full-resolution-image__link"
+              target="_blank"
+              title="View High Resolution Image"
+            >
+              View High Res
+            </a>{" "}
+          </figcaption>
+        </figure>
+        <h2 className="motivations__heading">What Intrigues Me</h2>
+        <p className="personal-info__text">{aboutUser.personalInfo1}</p>
+        <p className="personal-info__text">{aboutUser.personalInfo2}</p>
+
+        <figure className="additional-image__block">
+          <img
+            src={`../images/${aboutUser?.additionalPhoto}`}
+            alt={`${aboutUser.displayName} personal photo`}
+            className="additional__image"
+          />
+          <figcaption>
+            {" "}
+            <a
+              href={aboutUser.additionalPhotoLink}
+              className="full-resolution-image__link"
+              target="_blank"
+              title="View High Resolution Image"
+            >
+              View High Res
+            </a>
+          </figcaption>
+        </figure>
+
+        <section className="about-quote__block">
+          {/* This will be brought in by the Quote component */}
+          <blockquote className="blockquote">
+            {" "}
+            <p className="blockquote-single-line__text">
+              {" "}
+              If you can't beat 'em, unfollow their Instagram account
+            </p>
+            <cite className="quote__cite">&#8208; Author</cite>
+          </blockquote>
+        </section>
+
+        <section className="testimonies__block">
+          <h2 className="testimony__heading">What Teammates Say</h2>
+          <section className="testimony-item__block">
+            <p className="accolade__text">Sometimes right!</p>
+            <p className="author__name">Bells Whistles</p>
+          </section>
+          <section className="testimony-item__block">
+            <p className="accolade__text">I can't believe he's not butter!</p>
+            <p className="author__name">Left Overs</p>
+          </section>
+          <section className="testimony-item__block">
+            <p className="accolade__text">This isn't where I parked my care!</p>
+            <p className="author__name">Roger Rabbit</p>
+          </section>
         </section>
       </section>
     </>
