325b92edfc719eb52342d39053b9f0a68ea3214c
Michael Wright <info@saymoremultimedia.com>
2022-01-26 11:53:15 -0600

    mw-create-home-and-hero-28 Got the random string to display individual taglines

--- src/components/HeroTagline.js
+++ src/components/HeroTagline.js
@@ -3,13 +3,13 @@ import { getRandomObj } from "./Helpers";
 
 export const HeroTagline = () => {
   const [heroTaglines, setHeroTaglines] = useState([]);
+  const [taglineTextArray, setTaglineTextArray] = useState([]);
 
   //   const getNewRandomTagline =
-  const taglinesTextArray = () => {
-    let newArray = [];
-    heroTaglines.map((taglineObj) => newArray.push(taglineObj.text));
-
-    return newArray;
+  const taglinesTextArray = (arrayOfTaglines) => {
+    let newArray = arrayOfTaglines.map((taglineObj) => taglineObj.text);
+    console.log(newArray);
+    setTaglineTextArray(newArray);
   };
 
   useEffect(() => {
@@ -17,17 +17,18 @@ export const HeroTagline = () => {
       .then((res) => res.json())
       .then((data) => {
         setHeroTaglines(data.heroTaglines);
-        taglinesTextArray();
+        taglinesTextArray(data.heroTaglines);
       });
   }, []);
 
   return (
     <>
-      {heroTaglines.map((tagLineObj) => (
+      <p>{getRandomObj(taglineTextArray)}</p>
+      {/* {heroTaglines.map((tagLineObj) => (
         <p key={tagLineObj.id} className="hero__tagline">
           {tagLineObj.text}
         </p>
-      ))}
+      ))} */}
     </>
   );
 };
