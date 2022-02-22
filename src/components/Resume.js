a35248e5ff8dc45ac25c69bc68601388f9afdba7
Michael Wright <info@saymoremultimedia.com>
2022-02-22 17:29:07 -0600

    mw-update-resume-content-93 Resume rewrote HTML for the work experience

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,10 +4,10 @@ import { faDownload } from "@fortawesome/pro-light-svg-icons";
 
 export const Resume = ({ portfolioAuthor }) => {
   return (
-    <article className="resume__block theme__dark">
+    <article id="resume" className="resume__block theme__dark">
       <h3 className="resume__heading text__all-caps">Résumé</h3>
       <h2 className="resume__title">Michael P. Wright</h2>
-      <p className="resume__subtitle">UI/UX FRONT-END DEVELOPER</p>
+      <p className="resume__subtitle text__all-caps">UI/UX Front-end Developer</p>
       <section className="download-resume__block">
         <a
           href={portfolioAuthor.resumeLink}
@@ -26,7 +26,7 @@ export const Resume = ({ portfolioAuthor }) => {
       </section>
       <section className="resume about__block">
         {" "}
-        <h4 className="resume__subheading about__heading text__all-caps">
+        <h4 className="resume__subheading about__heading text__all-caps" title="About Me">
           About
         </h4>{" "}
         <p className="about__text">
@@ -35,11 +35,10 @@ export const Resume = ({ portfolioAuthor }) => {
           building a Design System, and defending my design choices.
         </p>
         <p className="about__text">
-          I’m seeking full-time and contract opportunities to expand my
-          knowledge and be a part of building really cool digital products.
+          I’m seeking full-time and contract opportunities as a UX Designer and Front-End Developer to design high impact web and mobile applications.
         </p>
       </section>
-      <section className="resume work-experience__block">
+      <section className="resume work-experience__block" title="Relevant Work Experience">
         {" "}
         <h4 className="resume__subheading work-experience__text text__all-caps">
           Work Experience
@@ -81,54 +80,22 @@ CSS, and JavaScript
             Produce visual &amp; interactive design with strategic use of typography,
 layout, color, motion, and behavior;
             </li>
-            <li className="work__list-item">
-              Project management/tracking with Github Projects &amp; Issue Tracking;
+            <li className="work__list-item">Convey interactive story ideas and complex interactions with wireframes
+or prototypes;
             </li>
-            <li className="work__list-item">Build single-page applications in the React framework leveraging HTML,
-CSS, and JavaScript
-            </li>
-          </ul>
-        </section>
-        <section className="work-experience__job">
-          <p className="work-section__title text__all-caps">
-            UI/UX FRONT-END DEVELOPER (APPRENTICESHIP)
-          </p>
-          <p className="work-section__location-name">
-            Nashville Software School
-          </p>
-          <p className="work-section__dates">2019-Present </p>
-
-          <ul className="work-section__bullet-points-list">
-            <li className="work__list-item">
-              Lorem ipsum dolor sit amet, consectetur{" "}
-            </li>
-            <li className="work__list-item">
-              Lorem ipsum dolor sit amet, consectetur{" "}
-            </li>
-            <li className="work__list-item">
-              Lorem ipsum dolor sit amet, consectetur{" "}
+            <li className="work__list-item">Plan user experience with intentional design around speed, efficiency
             </li>
           </ul>
         </section>
         <section className="work-experience__job">
-          <p className="work-section__title text__all-caps">
-            UI/UX FRONT-END DEVELOPER (APPRENTICESHIP)
-          </p>
-          <p className="work-section__location-name">
-            Nashville Software School
-          </p>
-          <p className="work-section__dates">2019-Present </p>
-
+          <p className="work-section__job-title">WordPress Website Designer</p>
+          <p className="work-section__location-name">Self-Employed | 2018 - 2021 | Huntsville, AL, USA</p>
           <ul className="work-section__bullet-points-list">
-            <li className="work__list-item">
-              Lorem ipsum dolor sit amet, consectetur{" "}
-            </li>
-            <li className="work__list-item">
-              Lorem ipsum dolor sit amet, consectetur{" "}
-            </li>
-            <li className="work__list-item">
-              Lorem ipsum dolor sit amet, consectetur{" "}
-            </li>
+            <li className="work__list-item">Design, develop, and launch websites build with WordPress CMS;</li>
+            <li className="work__list-item">Interview clients, perform Content Inventory, and website development
+milestones;</li>
+            <li className="work__list-item">Manage, backup/restore, update, and migrate website content including user
+account management, plug-in updates, and WordPress Core update</li>
           </ul>
         </section>
       </section>
