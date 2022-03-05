e874d051adb46b56b2714d23d3d248ac241baa8f
Michael Wright <info@saymoremultimedia.com>
2022-03-05 13:41:38 -0600

    mw-style-stick-navBar-65 updated meta description and manifest.json file with more specific, relevant to job-seeking information

--- src/components/Helpers.js
+++ src/components/Helpers.js
@@ -28,11 +28,3 @@ export const sortObjectsByNameProperty = (array) => {
   return sortedArray;
 };
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-export const addStickyClass = () => {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
-};
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/components/NavBar.js",
+    "./src/components/HeaderLogo.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
